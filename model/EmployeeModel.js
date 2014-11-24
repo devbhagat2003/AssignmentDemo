8d918c1db019f5000a39e230007b108ddc9088bd
dev <devbhagat2003@gmail.com>
2014-11-24 08:56:03 +0530

    First commit

---
+++ model/EmployeeModel.js
@@ -0,0 +1,14 @@
+﻿Assignment.service('EmployeeModel', function () {
+    //debugger;
+    this.getEmployees = function () {
+        return [
+            {
+                id: 0,
+                name: 'Dev Bhagat',
+                location: 'Chandigath, Sector 37A',
+                dateOfBirth: '12/02/1979',
+                joiningDate: '1/1/2014'
+            } 
+        ];
+    }
+});
